fd2b5ed65e93d7a7e4d9d78b34b2fe1a1621cd92
milkers <ilker1486@gmail.com>
2014-01-06 22:50:48 +0200

    there is a little glitch at getimages.php

--- getimages.php
+++ getimages.php
@@ -1,10 +1,16 @@
-<?
+<?php
 	//PHP SCRIPT: getimages.php
 	Header("content-type: application/x-javascript");
 
+	$dirname = "./last_study_data/".$_POST['imageDir'];
+	$dirvar = "./last_study_data/anger_autumn";
+	// echo "DIRECTORY is: ./last_study_data/".$_POST['imageDir'];
 	//This function gets the file names of all images in the current directory
 	//and ouputs them as a JavaScript array
-	function returnimages($dirname='./last_study_data/autumn') {
+	function returnimages($dirname="./last_study_data/anger_autumn") {
+
+		echo 'DDDDDDDDDDD'.$dirname;
+
 		$pattern="(\.jpg$)|(\.png$)|(\.jpeg$)|(\.gif$)"; //valid image extensions
 		$files = array();
 		$curimage=0;
@@ -12,7 +18,7 @@
 			while(false !== ($file = readdir($handle))){
 				if(eregi($pattern, $file)){ //if this file is a valid image
 					//Output it as a JavaScript array element
-					echo 'galleryarray['.$curimage.']="'.$file .'";';
+					echo ' galleryarray['.$curimage.']="'.$file .'";';
 					$curimage++;
 				}
 			}
@@ -23,5 +29,22 @@
 	}
 
 	echo 'var galleryarray=new Array();'; //Define array in JavaScript
-	returnimages() //Output the array elements containing the image file names
+
+	// $pattern="(\.jpg$)|(\.png$)|(\.jpeg$)|(\.gif$)"; //valid image extensions
+	// $files = array();
+	// $curimage=0;
+	// if($handle = opendir($dirname)) {
+	// 	while(false !== ($file = readdir($handle))){
+	// 		if(eregi($pattern, $file)){ //if this file is a valid image
+	// 			//Output it as a JavaScript array element
+	// 			echo 'galleryarray['.$curimage.']="'.$file.'";';
+	// 			$curimage++;
+	// 		}
+	// 	}
+
+	// 	closedir($handle);
+	// }	
+	
+	// returnimages("./last_study_data/".$_POST['imageDir']); //Output the array elements containing the image file names
+	returnimages();
 ?> 
